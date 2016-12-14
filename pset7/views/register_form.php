dff62724a29e67ab6a414708e86cc21cb806c0d9
Vishnu Mv <vishnum1998@gmail.com>
2016-12-14 12:18:38 +0000

    changes to portfolio

--- pset7/views/register_form.php
+++ pset7/views/register_form.php
@@ -7,13 +7,10 @@
             <input class="form-control" name="password" placeholder="Password" type="password"/>
         </div>
         <div class="form-group">
-            <input class="form-control" name="confirmation" placeholder="Confirm your Password" type="password"/>
+            <input class="form-control" name="confirmation" placeholder="Password (again)" type="password"/>
         </div>
         <div class="form-group">
-            <button class="btn btn-default" type="submit">
-                <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
-                Register
-            </button>
+            <button class="btn btn-default" type="submit">Register</button>
         </div>
     </fieldset>
 </form>
