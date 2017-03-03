6dfe5d94a3f8149192627c5cb7d76e9ae056b903
Vishnu Mv <vishnum1998@gmail.com>
2017-03-03 12:47:40 +0000

    sample curl code

---
+++ project1/curl.php
@@ -0,0 +1,10 @@
+<?php
+$url="http://www.shiksha.com/b-tech/colleges/b-tech-colleges-agartala";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+$html = curl_exec($ch);
+var_dump($html);
+curl_close($ch);
+?>
